e6692c5dbb6ca9f8258a4e74666250a1854d621d
burakathens <burakathens@outlook.com>
2021-04-18 23:33:05 +0300

    18 nisan odevleri eklendi. CodingBatOdevler

--- src/JavAthens/Week3/CodingBatOdevler.java
+++ src/JavAthens/Week3/CodingBatOdevler.java
@@ -2,7 +2,47 @@ package JavAthens.Week3;
 
 public class CodingBatOdevler {
     public static void main(String[] args) {
+        /*
 
+            Burak's Note>> odevleri kendi branch ınızda olusturdugunuz
+            class larda cozup kendinize push edeceksiniz, daha sonra pull request olusturacaksınız
+            biz merge ettikten sonra tüm projeyi pull edebilirsiniz.
+            not2: master rolünden kesinlikle push yapmıyoruz :) tesekkurler...
+
+        * *******************
+        *   18 NİSAN ODEVLERİ
+        *
+        * ********************* */
+        /* LOGIC-1 : cigarParty -
+        * When squirrels get together for a party, they like to have cigars.
+        * A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+        * Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+        * Return true if the party with the given values is successful, or false otherwise.
+
+        * cigarParty(30, false) → false
+        * cigarParty(50, false) → true
+        * cigarParty(70, true) → true
+        *
+        * ************************* */
+        /* ARRAY-1 : plusTwo
+        * Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+        *
+        *   plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+        *   plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+        *   plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+        *
+        *
+        *********************** */
+
+        /* STRING-1 right2
+        * Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+        * The string length will be at least 2.
+        *
+        * right2("Hello") → "loHel"
+        * right2("java") → "vaja"
+        * right2("Hi") → "Hi"
+        *
+        * ********************** */
 
 
 
